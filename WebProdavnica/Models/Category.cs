bf14213c6910d8c76a551a8b8f1d00c4a24da762
Milos <Milos@MILOS>
2024-12-10 23:43:52 +0100

    Add project files.

---
+++ WebProdavnica/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebProdavnica.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        [Required]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+
+    }
+}
